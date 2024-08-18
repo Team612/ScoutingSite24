bc2cb3def329db0556f8d748f8f1fad4435a395a
Siri <siriyakkala28@gmail.com>
2024-08-18 15:29:03 -0400

    nav

--- vite-project/src/NavElements.jsx
+++ vite-project/src/NavElements.jsx
@@ -4,32 +4,33 @@ import styled from "styled-components";
 /* This page should only be for switching between pages / for buttons */
 const Nav = styled(Link)
 `
-margin:3.5vh 1.4vw;
-/* padding: 1vw; */
-float:right;
-background-color: #000000;
+margin: ${props => (props.isClassInput ? "3em 0px 3em 0px" : props.isHomeButtonPitScouting ? "3.25vh 0vw 3.25 vh 80vw" : "3.5vh 1.4vw")};
+padding: ${props => (props.isClassInput ? "10px" : "0px")};
+float: ${props => (props.isClassInput ? "none" : "right")};
+background-color: ${props => (props.isClassInput ? "rgb(53, 163, 222)" : props.isHomeButtonPitScouting ? "#0e24a1" : "#000000")};
 border-style: none;
-box-sizing: border-box;
+box-sizing: ${props => (props.isClassInput ? "content-box" : props.isHomeButtonPitScouting ? "content-box" : "border-box")};
 color: #FFFFFF;
 cursor: pointer;
 display: inline-block;
-font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
-font-size: 4vw;
+font-family: ${props => (props.isClassInput ? "'Montserrat', sans-serif" : props.isHomeButtonPitScouting ? "-apple-system,system-ui,'Segoe UI',Roboto,'Helvetica Neue',Ubuntu,sans-serif" : "'Haas Grot Text R Web', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'")};
+font-size: ${props => (props.isClassInput ? "100%" : props.isHomeButtonPitScouting ? "200%" : "4vw")};
 font-weight: 500;
-height: 5.8vh;
+height: ${props => (props.isClassInput ? "auto" : props.isHomeButtonPitScouting ? "8vh" : "5.8vh")};
 list-style: none;
-/* margin: 0; */
 outline: none;
-position: relative;
-text-align: center;
+position: ${props => (props.isClassInput ? "static" : "relative")};
+text-align: ${props => (props.isClassInput ? "start" : "center")};
 text-decoration: none;
 /* transition: color 100ms; */
 vertical-align: baseline;
-user-select: none;
--webkit-user-select: none;
-touch-action: manipulation;
+user-select: ${props => (props.isClassInput ? "auto" : "none")};
+-webkit-user-select: ${props => (props.isClassInput ? "text" : "none")};
+touch-action: ${props => (props.isClassInput ? "auto" : "manipulation")};
+border: none;
+border-radius: ${props => (props.isClassInput ? "10px" : props.isHomeButtonPitScouting ? "1.5vw" : "0")};
+width: ${props => (props.isClassInput ? "20vh" : props.isHomeButtonPitScouting ? "8vw" : "auto")};
 
 
 `;
-
 export default Nav;
